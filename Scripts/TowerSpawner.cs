1870ec99b64111b21f9a5406505d44a17a9eb792
GiritClap <moonjun210@naver.com>
2022-07-24 14:04:30 +0900

    upload basic
    upload basic

--- Scripts/TowerSpawner.cs
+++ Scripts/TowerSpawner.cs
@@ -12,14 +12,38 @@ public class TowerSpawner : MonoBehaviour
     private int towerBuildGold = 30; // 타워 건설 비용
     [SerializeField]
     private PlayerGold playerGold; //타워 건설 시 골드 감소
+    private bool isOnTowerButton = false; // 타워 건설 버튼을 눌렀는지 체크
+    [SerializeField]
+    private GameObject followTower; // 임시 타워 사용 완료 시 삭제를 위해 저장하는 변수
+    private GameObject followTowerClone;
 
-    public void SpawnTower(Transform tileTransform)
+    public void ReadyToSpawnTower()
     {
-        // 타워를 건설할 만큼 돈이 없다면 건설x
+        if (isOnTowerButton == true)
+        {
+            return;
+        }
+
         if (towerBuildGold > playerGold.CurrentGold)
         {
             return;
         }
+
+        isOnTowerButton = true;
+        followTowerClone = Instantiate(followTower);
+    }
+
+    public void SpawnTower(Transform tileTransform)
+    {
+        if(isOnTowerButton == false)
+        {
+            return ;
+        }
+        // 타워를 건설할 만큼 돈이 없다면 건설x
+        //if (towerBuildGold > playerGold.CurrentGold)
+        //{
+        //    return;
+       // }
         Tile tile = tileTransform.GetComponent<Tile>();
 
         // 타워 건설 가능여부 확인
@@ -31,10 +55,12 @@ public class TowerSpawner : MonoBehaviour
 
         // 타워가 설치되어 있으므로 설정
         tile.IsBuildTower = true;
+        isOnTowerButton = false;
         // 타워 설치 시 돈 감소
         playerGold.CurrentGold -= towerBuildGold;
         // 선택한 타일의 위치에 타워 건설
         GameObject clone = Instantiate(towerPrefab, tileTransform.position, Quaternion.identity);
         clone.GetComponent<TowerWeapon>().SetUp(enemySpawner);
+        Destroy(followTowerClone);
     }
 }
